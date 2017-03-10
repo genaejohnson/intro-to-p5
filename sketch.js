bf68b91f43f00c1ce86b895e2421c0b236fc7179
Genae Johnson <gsjohnson@wlapcs.org>
2017-03-10 18:16:12 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup(){
+   createCanvas(600,400); 
+  background(100,100,100);
+}
+
+function draw(){
+   
+   line(35, 80, 300, 200);
+   
+   stroke(255,255,255);
+   fill(66,217,244);
+   rect(290,100,250,250);
+   
+   stroke(0,0,0);
+   strokeWeight(4);
+   fill(89,244,66)
+   ellipse(50,250,80,80);
+   
+   
+}
+
+//draw me 2 ellipse
+//draw me 2 rectangles
+//draw me 4 lines (1 with a positive slope, 1 with a neg slope,)
+//1 with a slope of 0, and 1 with a slope of undefined.
